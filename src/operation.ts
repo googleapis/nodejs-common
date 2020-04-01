73ab21a487871861a2b7de5ea980bac9ed440d42
yoshi-automation <yoshi-automation@google.com>
2020-04-01 04:42:03 -0700

    chore!: drop support for node.js 8 (#554)
    https://github.com/googleapis/nodejs-common/commit/9f410477432893f68e57b5eeb31a068a3d8ef52f
    commit 9f410477432893f68e57b5eeb31a068a3d8ef52f
    Author: Justin Beckwith <justin.beckwith@gmail.com>
    Date:   Thu Mar 26 12:11:44 2020 -0700
    
        chore!: drop support for node.js 8 (#554)

--- src/operation.ts
+++ src/operation.ts
@@ -1,18 +1,16 @@
-/*!
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2016 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /*!
  * @module common/operation
@@ -27,7 +25,7 @@ import {
 import {ApiError} from './util';
 import {promisify} from 'util';
 
-// tslint:disable-next-line no-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class Operation<T = any> extends ServiceObject<T> {
   completeListeners: number;
   hasActiveListeners: boolean;
@@ -71,7 +69,7 @@ export class Operation<T = any> extends ServiceObject<T> {
       config
     );
 
-    // tslint:disable-next-line:no-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     config.methods = (config.methods || methods) as any;
     super(config);
     this.completeListeners = 0;
--- test/operation.ts
+++ test/operation.ts
@@ -1,21 +1,19 @@
-/*!
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2016 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import * as assert from 'assert';
-import {describe, it} from 'mocha';
+import {describe, it, beforeEach, afterEach} from 'mocha';
 import * as sinon from 'sinon';
 
 import {Service} from '../src';
@@ -27,7 +25,7 @@ import {
 } from '../src/service-object';
 import {util} from '../src/util';
 
-// tslint:disable-next-line no-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const asAny = (o: {}) => o as any;
 
 describe('Operation', () => {
@@ -80,9 +78,9 @@ describe('Operation', () => {
     });
 
     it('should call listenForEvents_', () => {
-      // tslint:disable-next-line no-any
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const stub = sandbox.stub(Operation.prototype as any, 'listenForEvents_');
-      const op = new Operation({parent} as ServiceObjectConfig);
+      new Operation({parent} as ServiceObjectConfig);
       assert.ok(stub.called);
     });
   });
@@ -274,10 +272,11 @@ describe('Operation', () => {
           return asAny({});
         });
 
-        asAny(operation).startPolling_ = function() {
+        asAny(operation).startPolling_ = function () {
           if (!startPollingCalled) {
             // Call #1.
             startPollingCalled = true;
+            // eslint-disable-next-line prefer-rest-params
             startPolling_.apply(this, arguments);
             return;
           }
@@ -307,10 +306,11 @@ describe('Operation', () => {
           return asAny({});
         });
 
-        asAny(op).startPolling_ = function() {
+        asAny(op).startPolling_ = function () {
           if (!startPollingCalled) {
             // Call #1.
             startPollingCalled = true;
+            // eslint-disable-next-line prefer-rest-params
             startPolling_.apply(this, arguments);
             return;
           }
