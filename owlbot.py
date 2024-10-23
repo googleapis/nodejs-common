cac2d263650eff38fe19522aaea6917cba92d16b
sofisl <55454395+sofisl@users.noreply.github.com>
2024-10-23 12:02:10 -0400

    Update owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -21,5 +21,5 @@ logging.basicConfig(level=logging.DEBUG)
 
 common_templates = gcp.CommonTemplates()
 templates = common_templates.node_library()
-s.copy(templates)
+s.copy(templates, excludes = [".github/ISSUE_TEMPLATE", ".github/workflows/issues-no-repro.yaml", ".github/scripts/close-invalid-link.cjs"])
 node.fix()
